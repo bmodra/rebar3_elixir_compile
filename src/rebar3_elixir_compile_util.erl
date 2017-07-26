1e1051deff3596cb44043930a26ef2eda8c2417f
Brian Modra <brian@medical-objects.com.au>
2017-07-26 10:06:46 +1000

    escaping paths for windows

--- src/rebar3_elixir_compile_util.erl
+++ src/rebar3_elixir_compile_util.erl
@@ -52,21 +52,38 @@ add_elixir(State) ->
     code:add_patha(filename:join(LibDir, "logger/ebin")),
     MixState.
 
+escape_path(Path) ->
+    lists:flatten(
+      [if
+           (X >= $a) and (X =< $z) -> X;
+           (X >= $A) and (X =< $Z) -> X;
+           (X >= $0) and (X =< $9) -> X;
+           X == $. -> X;
+           X == $\- -> X;
+           X == $_ -> X;
+           X == $/ -> X;
+           true -> [$\\, X ]
+       end || X <- Path ]).
+
 get_details(State) ->
     Config = rebar_state:get(State, elixir_opts),
-    BinDir = [$"] ++ case lists:keyfind(bin_dir, 1, Config) of
-                false -> 
-                    {ok, ElixirBin_} = find_executable("elixir"),
-                    filename:dirname(ElixirBin_);
-                {bin_dir, Dir1} -> Dir1
-             end ++ [$"], 
-
-    LibDir = [$"] ++ case lists:keyfind(lib_dir, 1, Config) of
-                false -> 
-                    {ok, ElixirLibs_} = rebar_utils:sh("elixir -e \"IO.puts :code.lib_dir(:elixir)\"", []),
-                    filename:join(re:replace(ElixirLibs_, "\\s+", "", [global,{return,list}]), "../");
-                {lib_dir, Dir2} -> Dir2
-             end ++ [$"],            
+    BinDir =
+        escape_path(
+          case lists:keyfind(bin_dir, 1, Config) of
+              false -> 
+                  {ok, ElixirBin_} = find_executable("elixir"),
+                  filename:dirname(ElixirBin_);
+              {bin_dir, Dir1} -> Dir1
+          end), 
+    
+    LibDir = 
+        escape_path(
+          case lists:keyfind(lib_dir, 1, Config) of
+              false -> 
+                  {ok, ElixirLibs_} = rebar_utils:sh("elixir -e \"IO.puts :code.lib_dir(:elixir)\"", []),
+                  filename:join(re:replace(ElixirLibs_, "\\s+", "", [global,{return,list}]), "../");
+              {lib_dir, Dir2} -> Dir2
+          end),
     {env, Env} = lists:keyfind(env, 1, Config),
     {BinDir, Env, Config, LibDir}.
 
